ed57a19d14a0b8f7de7cc303cfa256111386e40a
Attila Nagy <attilanagy85@gmail.com>
2018-10-30 21:50:50 +0000

    script to turn off used gpio pins

---
+++ test/turn_off_gpio.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+
+SHUTX_PIN_1 = 20
+SHUTX_PIN_2 = 16
+
+GPIO.setwarnings(False)
+
+# Setup GPIO for shutdown pins on
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SHUTX_PIN_1, GPIO.OUT)
+GPIO.setup(SHUTX_PIN_2, GPIO.OUT)
+
+# Reset sensor
+GPIO.output(SHUTX_PIN_1, GPIO.LOW)
+GPIO.output(SHUTX_PIN_2, GPIO.LOW)
